d504d06aa3c5e7b201d2981531805741064ac4c2
Aiden <126488989+AidenRIreland@users.noreply.github.com>
2024-11-11 20:50:20 -0500

    new route to get users online status

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUsersForSidebar, updateUser, getUserById } from "../controllers/user.controller.js";
+import { getUsersForSidebar, updateUser, getUserById, getUserStatus} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getUsersForSidebar);
 router.put("/update", protectRoute, updateUser);
 router.get("/:id", protectRoute, getUserById);
+
+//*New Route to get the users online status
+router.get("/status/:id", protectRoute, getUserStatus)
 
 export default router;
