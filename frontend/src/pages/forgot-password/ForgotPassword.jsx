5a9d4e9742887ce36e3350f1b70b13336454a8db
Aiden <126488989+AidenRIreland@users.noreply.github.com>
2024-12-02 20:54:39 -0500

    Fixed Design

--- frontend/src/pages/forgot-password/ForgotPassword.jsx
+++ frontend/src/pages/forgot-password/ForgotPassword.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-
+import { useState } from "react";
 import axios from "axios";
 
 const ForgotPassword = () => {
@@ -16,6 +15,7 @@ const ForgotPassword = () => {
         { username }
       );
       setTwoFactorEnabled(response.data.twoFactorEnabled);
+      setMessage("");
     } catch (error) {
       setMessage("Error checking 2FA status");
     }
@@ -34,43 +34,86 @@ const ForgotPassword = () => {
         payload
       );
 
-      setMessage(response.data.message);
+      setMessage(response.data.message || "Password reset successfully");
     } catch (error) {
       setMessage(error.response?.data?.error || "Error resetting password");
     }
   };
 
   return (
-    <div>
-      <h1>Forgot Password</h1>
-      <input
-        type="text"
-        placeholder="Enter your username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <button onClick={handleCheck2FA}>Check 2FA Status</button>
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-white">
+        <h1 className="text-3xl font-semibold text-center text-gray-500">
+          Forgot <span className="text-[#5A68D8]">Password</span>
+        </h1>
 
-      {twoFactorEnabled !== null && (
-        <>
-          {twoFactorEnabled && (
-            <input
-              type="text"
-              placeholder="Enter 2FA Code"
-              value={token}
-              onChange={(e) => setToken(e.target.value)}
-            />
-          )}
+        {/* Display message */}
+        {message && (
+          <p className="text-sm text-center text-red-500 my-2">{message}</p>
+        )}
+
+        <div>
+          <label className="label p-2">
+            <span className="text-base label-text text-gray-500">
+              Enter your username
+            </span>
+          </label>
           <input
-            type="password"
-            placeholder="Enter your new password"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
+            type="text"
+            placeholder="Enter your username"
+            className="w-full input input-bordered h-10 bg-[#f0f0f0] text-gray-500 rounded-md"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
-          <button onClick={handleResetPassword}>Reset Password</button>
-        </>
-      )}
-      {message && <p>{message}</p>}
+          <button
+            className="btn btn-block btn-sm mt-2 bg-[#5A68D8] text-white"
+            onClick={handleCheck2FA}
+          >
+            Check 2FA Status
+          </button>
+        </div>
+
+        {twoFactorEnabled !== null && (
+          <>
+            {twoFactorEnabled && (
+              <div>
+                <label className="label p-2">
+                  <span className="text-base label-text text-gray-500">
+                    Enter 2FA Code
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter 2FA Code"
+                  className="w-full input input-bordered h-10 bg-[#f0f0f0] text-gray-500 rounded-md"
+                  value={token}
+                  onChange={(e) => setToken(e.target.value)}
+                />
+              </div>
+            )}
+            <div>
+              <label className="label p-2">
+                <span className="text-base label-text text-gray-500">
+                  New Password
+                </span>
+              </label>
+              <input
+                type="password"
+                placeholder="Enter your new password"
+                className="w-full input input-bordered h-10 bg-[#f0f0f0] text-gray-500 rounded-md"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+            </div>
+            <button
+              className="btn btn-block btn-sm mt-2 bg-[#5A68D8] text-white"
+              onClick={handleResetPassword}
+            >
+              Reset Password
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
